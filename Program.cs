70434b5bc19741cfdc2a1d1e2d34a30f17c7b0ba
TitasSp <titas.spegis@mif.stud.vu.lt>
2025-09-21 18:43:35 +0300

    simple hash
    manipulates bytes to get unique string

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Hash
+{
+    static int Main()
+    {
+        Console.WriteLine(Mixing("Lietuva", 32));
+        return 0;
+    }
+
+    static string Mixing(string input, int output_size)
+    {
+        byte[] output = new byte[output_size];
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            byte ch = (byte)input[i];
+
+            for (int j = 0; j < output_size; j++)
+            {
+                output[j] ^= (byte)((ch + j * 13) & 0xFF);
+                output[j] = (byte)((output[j] << 3) | (output[j] >> 5));
+                output[j] = (byte)((output[j] + ch + i) & 0xFF);
+            }
+        }
+
+        //convert from byte array to string
+        char[] c = new char[output.Length * 2];
+        int b;
+        for (int i = 0; i < output.Length; i++)
+        {
+            b = output[i] >> 4;
+            c[i * 2] = (char)(55 + b + (((b - 10) >> 31) & -7));
+            b = output[i] & 0xF;
+            c[i * 2 + 1] = (char)(55 + b + (((b - 10) >> 31) & -7));
+        }
+
+        return new string(c);
+    }
+}
